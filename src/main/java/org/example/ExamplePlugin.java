295eacf97fa2b0a2ba989cd5841f0058ae96d87c
WowStarWorld <starworldstudio1@gmail.com>
2023-10-29 21:27:37 +0800

    更新SharkBot版本至1.0.3

--- src/main/java/org/example/ExamplePlugin.java
+++ src/main/java/org/example/ExamplePlugin.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import team.starworld.shark.SharkBotApplication;
 import team.starworld.shark.data.plugin.Plugin;
 import team.starworld.shark.event.application.plugin.PluginLoadEvent;
-import team.starworld.shark.event.application.resources.ResourceLoadFinishEvent;
+import team.starworld.shark.event.application.resource.AllResourceLoadedEvent;
 import team.starworld.shark.network.chat.Component;
 
 @SuppressWarnings("unused")
@@ -16,10 +16,10 @@ public class ExamplePlugin {
     public static final Logger LOGGER = LoggerFactory.getLogger(ExamplePlugin.class);
 
     public ExamplePlugin (PluginLoadEvent event) {
-        SharkBotApplication.RESOURCE_LOADER.eventBus.once(ResourceLoadFinishEvent.class, this::onResourceLoadFinish);
+        SharkBotApplication.RESOURCE_LOADER.eventBus.once(AllResourceLoadedEvent.class, this::onAllResourceLoaded);
     }
 
-    private void onResourceLoadFinish (ResourceLoadFinishEvent event) {
+    private void onAllResourceLoaded (AllResourceLoadedEvent event) {
         LOGGER.info(Component.translatable("example_plugin.message").getString());
     }
 
